ad32d25ac9aa194bd55c713ca044b679e8540912
Armon Antolin <74826437+JArmonTeach@users.noreply.github.com>
2023-10-12 21:53:23 -0400

    build: connecting and tested API - status: working

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
-import React from 'react';
+import {React, useEffect} from 'react';
+
+const API_URL = 'http://www.omdbapi.com?apikey=e1784924';
 
 const App = () => {
+
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
+
+        console.log(data.Search);
+    }
+
+    useEffect(() => {
+        searchMovies('Indiana Jones');
+    }, []);
+
     return (
-        <h1></h1>
+        <h1>App</h1>
     );
 }
 
